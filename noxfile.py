d4276d52af17b6178c99729d9e4df85d2331b30a
qingxiangguo <johnleberand@hotmail.com>
2023-11-14 22:54:54 -0600

    Revise noxfile to resolve Python version issue

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,11 @@
 import nox
 
-@nox.session(python=["3.9", "3.10"])
-def tests(session):
+@nox.session(python="3.9")
+def tests_39(session):
+    session.run("poetry", "install", external=True)
+    session.run("pytest")
+
+@nox.session(python="3.10")
+def tests_310(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
