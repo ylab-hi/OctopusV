3f5d9d5b9ae52078cc73534ae59dab41401c681b
qingxiangguo <johnleberand@hotmail.com>
2023-11-15 14:52:15 -0600

    Fix coverage package install

--- noxfile.py
+++ noxfile.py
@@ -13,4 +13,4 @@ def tests_310(session):
 @nox.session
 def coverage(session):
     session.install("coverage")
-    session.run("coverage", "html", "-i", "--from-coverage=coverage.xml")
+    session.run("coverage", "html")
