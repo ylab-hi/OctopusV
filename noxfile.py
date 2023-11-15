60b28d50c254621c8377ae65a4dc610d0ad6e158
qingxiangguo <johnleberand@hotmail.com>
2023-11-15 11:14:34 -0600

    Revise noxfile to resolve coverage generate

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,9 @@ import nox
 @nox.session(python="3.9")
 def tests_39(session):
     session.run("poetry", "install", external=True)
-    session.run("pytest")
+    session.run("pytest", "--cov=octopusv")
 
 @nox.session(python="3.10")
 def tests_310(session):
     session.run("poetry", "install", external=True)
-    session.run("pytest")
+    session.run("pytest", "--cov=octopusv")
