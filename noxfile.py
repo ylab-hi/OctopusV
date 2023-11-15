349720e7756a4278bf3523a22a2707304ef3191c
qingxiangguo <johnleberand@hotmail.com>
2023-11-15 12:56:51 -0600

    Fix coverage uploading

--- noxfile.py
+++ noxfile.py
@@ -9,3 +9,11 @@ def tests_39(session):
 def tests_310(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov", "--cov-report=xml")
+
+@nox.session
+def coverage(session):
+    # Combine the downloaded coverage data.
+    session.run("coverage", "combine", "--append")
+    # Output report
+    session.run("coverage", "report")
+    session.run("coverage", "html")
