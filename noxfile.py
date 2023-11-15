5296b6e7cf96fb6c26bf5496649b7a5e667d1abe
qingxiangguo <johnleberand@hotmail.com>
2023-11-15 15:45:47 -0600

    Fix coverage package install

--- noxfile.py
+++ noxfile.py
@@ -3,14 +3,9 @@ import nox
 @nox.session(python="3.9")
 def tests_39(session):
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov", "--cov-report=xml")
+    session.run("pytest", "--cov", "--cov-report=html")
 
 @nox.session(python="3.10")
 def tests_310(session):
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov", "--cov-report=xml")
-
-@nox.session
-def coverage(session):
-    session.install("coverage")
-    session.run("coverage", "html", "-i")
+    session.run("pytest", "--cov", "--cov-report=html")
