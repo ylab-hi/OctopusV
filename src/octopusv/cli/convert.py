37d0a27fdcad5dd8d71405d915c5b575a622906d
qingxiangguo <johnleberand@hotmail.com>
2024-12-02 15:33:27 -0600

    Fix: BND tolerance

--- src/octopusv/cli/convert.py
+++ src/octopusv/cli/convert.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import logging
 import typer
 
 from octopusv.converter.base import get_alt_chrom_pos
@@ -24,22 +24,23 @@ from octopusv.utils.svcf_utils import write_sv_vcf
 
 
 def correct(
-    input_vcf: Path | None = typer.Argument(
-        None, exists=True, dir_okay=False, resolve_path=True, help="Input VCF file to correct."
-    ),
-    output: Path | None = typer.Argument(None, dir_okay=False, resolve_path=True, help="Output file path."),
-    input_option: Path | None = typer.Option(
-        None, "--input-file", "-i", exists=True, dir_okay=False, resolve_path=True, help="Input VCF file to correct."
-    ),
-    output_option: Path | None = typer.Option(
-        None, "--output-file", "-o", dir_okay=False, resolve_path=True, help="Output file path."
-    ),
-    pos_tolerance: int = typer.Option(
-        3,
-        "--pos-tolerance",
-        "-pt",
-        help="Position tolerance for identifying mate BND events, default=3, recommend not to set larger than 5",
-    ),
+        input_vcf: Path | None = typer.Argument(
+            None, exists=True, dir_okay=False, resolve_path=True, help="Input VCF file to correct."
+        ),
+        output: Path | None = typer.Argument(None, dir_okay=False, resolve_path=True, help="Output file path."),
+        input_option: Path | None = typer.Option(
+            None, "--input-file", "-i", exists=True, dir_okay=False, resolve_path=True,
+            help="Input VCF file to correct."
+        ),
+        output_option: Path | None = typer.Option(
+            None, "--output-file", "-o", dir_okay=False, resolve_path=True, help="Output file path."
+        ),
+        pos_tolerance: int = typer.Option(
+            3,
+            "--pos-tolerance",
+            "-pt",
+            help="Position tolerance for identifying mate BND events, default=3, recommend not to set larger than 5",
+        ),
 ):
     """Correct SV events."""
     # Determine input file
@@ -70,90 +71,58 @@ def correct(
         typer.echo("Error: Output file is required.", err=True)
         raise typer.Exit(code=1)
 
-    # Ensure input_file and output_file are not None
-    if input_file is None:
-        typer.echo("Error: Input file is not specified.", err=True)
-        raise typer.Exit(code=1)
-    if output_file is None:
-        typer.echo("Error: Output file is not specified.", err=True)
-        raise typer.Exit(code=1)
-
     # Parse the input VCF file
     # non_bnd_events means DEL, INV, INS, DUP
-    contig_lines, same_chr_bnd_events, diff_chr_bnd_events, non_bnd_events = parse_vcf(
-        input_file,
-    )
+    contig_lines, same_chr_bnd_events, diff_chr_bnd_events, non_bnd_events = parse_vcf(input_file)
 
     # Extract mate BND and no mate events, they are all with different chromosomes
-    mate_bnd_pairs = find_mate_bnd_events(
-        diff_chr_bnd_events,
-        pos_tolerance=pos_tolerance,
-    )
-
-    no_mate_events = find_no_mate_events(
-        diff_chr_bnd_events,
-        pos_tolerance=pos_tolerance,
-    )
+    mate_bnd_pairs = find_mate_bnd_events(diff_chr_bnd_events, pos_tolerance=pos_tolerance)
+    no_mate_events = find_no_mate_events(diff_chr_bnd_events, pos_tolerance=pos_tolerance)
 
     # Further classify no_mate_events into special_no_mate_diff_bnd_pair and other_single_TRA
-    (
-        special_no_mate_diff_bnd_pairs,
-        other_single_TRA,
-    ) = find_special_no_mate_diff_bnd_pair_and_other_single_tra(
+    special_no_mate_diff_bnd_pairs, other_single_TRA = find_special_no_mate_diff_bnd_pair_and_other_single_tra(
         no_mate_events,
         pos_tolerance=pos_tolerance,
     )
 
     # Initialize the EventTransformer with a list of transform strategies for each type of events
-    # One transformer and multiple-strategies
-    same_chr_bnd_transformer = SameChrBNDTransformer(
-        [
-            BND_to_INV_Converter(),
-            BND_to_DUP_Converter(),
-            BND_to_TRA_Forward_Converter(),
-            BND_to_TRA_Reverse_Converter(),
-        ],
-    )
-    mate_bnd_pair_transformer = MatePairBNDTransformer(
-        [
-            MatePairReciprocalTranslocationToTRAConverter(),
-            MatePairIndependentToTRAConverter(),
-            MatePairMergeToTRAConverter(),
-        ],
-    )
-    special_no_mate_diff_bnd_pair_transformer = SpecialNoMateDiffBNDPairTransformer(
-        [
-            SpecialNoMateDiffBNDPairReciprocalTranslocationToTRAConverter(),
-            SpecialNoMateDiffBNDPairIndependentToTRAConverter(),
-        ],
-    )
+    same_chr_bnd_transformer = SameChrBNDTransformer([
+        BND_to_INV_Converter(),
+        BND_to_DUP_Converter(),
+        BND_to_TRA_Forward_Converter(),
+        BND_to_TRA_Reverse_Converter(),
+    ])
+
+    mate_bnd_pair_transformer = MatePairBNDTransformer([
+        MatePairReciprocalTranslocationToTRAConverter(),
+        MatePairIndependentToTRAConverter(),
+        MatePairMergeToTRAConverter(),
+    ])
+
+    special_no_mate_diff_bnd_pair_transformer = SpecialNoMateDiffBNDPairTransformer([
+        SpecialNoMateDiffBNDPairReciprocalTranslocationToTRAConverter(),
+        SpecialNoMateDiffBNDPairIndependentToTRAConverter(),
+    ])
+
     single_TRA_transformer = SingleTRATransformer([SingleTRAToTRAConverter()])
-    non_bnd_transformer = NonBNDTransformer(
-        [NonBNDConverter()],
-    )  # Assuming non-BND events are not to be transformed for now
+    non_bnd_transformer = NonBNDTransformer([NonBNDConverter()])
 
     # Apply all transformation strategies to the events
-    same_chr_bnd_transformed_events = same_chr_bnd_transformer.apply_transforms(
-        same_chr_bnd_events,
-    )
-    mate_pair_transformed_events = mate_bnd_pair_transformer.apply_transforms(
-        mate_bnd_pairs,
-    )
+    same_chr_bnd_transformed_events = same_chr_bnd_transformer.apply_transforms(same_chr_bnd_events)
+    mate_pair_transformed_events = mate_bnd_pair_transformer.apply_transforms(mate_bnd_pairs)
     special_no_mate_diff_bnd_pair_transformed_events = special_no_mate_diff_bnd_pair_transformer.apply_transforms(
-        special_no_mate_diff_bnd_pairs,
+        special_no_mate_diff_bnd_pairs
     )
-    single_TRA_transformed_events = single_TRA_transformer.apply_transforms(
-        other_single_TRA,
-    )
+    single_TRA_transformed_events = single_TRA_transformer.apply_transforms(other_single_TRA)
     non_bnd_transformed_events = non_bnd_transformer.apply_transforms(non_bnd_events)
 
     # Merge all transformed events
     all_transformed_events = (
-        same_chr_bnd_transformed_events
-        + mate_pair_transformed_events
-        + special_no_mate_diff_bnd_pair_transformed_events
-        + single_TRA_transformed_events
-        + non_bnd_transformed_events
+            same_chr_bnd_transformed_events
+            + mate_pair_transformed_events
+            + special_no_mate_diff_bnd_pair_transformed_events
+            + single_TRA_transformed_events
+            + non_bnd_transformed_events
     )
 
     # Write the transformed events to the output file
@@ -161,114 +130,130 @@ def correct(
     typer.echo(f"Corrected SV events written to {output_file}")
 
 
-# ==============================
-# The following are auxiliary Functions.
-# ==============================
 def find_mate_bnd_events(events, pos_tolerance=3):
     """Find mate BND events."""
     event_dict = {}
     mate_bnd_pairs = []
 
     for event in events:
-        chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
-        key_for_searching = (event.chrom, event.pos, chrom_alt, pos_alt)
+        try:
+            chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
+            if chrom_alt is None or pos_alt is None:
+                continue
 
-        # Generate all possible reverse keys
-        possible_reverse_keys = [
-            (chrom_alt, pos_alt + i, event.chrom, event.pos + j)
-            for i in range(-pos_tolerance, pos_tolerance + 1)
-            for j in range(-pos_tolerance, pos_tolerance + 1)
-        ]
+            key_for_searching = (event.chrom, event.pos, chrom_alt, pos_alt)
 
-        for reverse_key in possible_reverse_keys:
-            if reverse_key in event_dict:
-                mate_bnd_pairs.append((event_dict.pop(reverse_key), event))
-                break
-        else:
-            event_dict[key_for_searching] = event
-            # It will override for no_mate_events due to the same key, but does matter.
-            # We only output mate_bnd_pairs here
+            # Generate all possible reverse keys
+            possible_reverse_keys = []
+            for i in range(-pos_tolerance, pos_tolerance + 1):
+                for j in range(-pos_tolerance, pos_tolerance + 1):
+                    try:
+                        new_key = (chrom_alt, pos_alt + i, event.chrom, event.pos + j)
+                        possible_reverse_keys.append(new_key)
+                    except TypeError:
+                        continue
+
+            for reverse_key in possible_reverse_keys:
+                if reverse_key in event_dict:
+                    mate_bnd_pairs.append((event_dict.pop(reverse_key), event))
+                    break
+            else:
+                event_dict[key_for_searching] = event
+
+        except (TypeError, ValueError) as e:
+            logging.info(f"Error processing event: {str(e)}")
+            continue
 
     return mate_bnd_pairs
 
 
 def find_no_mate_events(events, pos_tolerance=3):
-    """Find no mate events. The main idea is to store events in list to avoid overriding in event_dict."""
+    """Find no mate events."""
     event_dict = {}
     mate_bnd_pairs = []
 
     for event in events:
-        chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
-        key = (event.chrom, event.pos, chrom_alt, pos_alt)
+        try:
+            chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
+            if chrom_alt is None or pos_alt is None:
+                continue
 
-        # Generate all possible reverse keys
-        possible_reverse_keys = [
-            (chrom_alt, pos_alt + i, event.chrom, event.pos + j)
-            for i in range(-pos_tolerance, pos_tolerance + 1)
-            for j in range(-pos_tolerance, pos_tolerance + 1)
-        ]
+            key = (event.chrom, event.pos, chrom_alt, pos_alt)
 
-        mate_found = False  # A flag to indicate if a matching mate has been found for the event.
+            # Generate all possible reverse keys
+            possible_reverse_keys = []
+            for i in range(-pos_tolerance, pos_tolerance + 1):
+                for j in range(-pos_tolerance, pos_tolerance + 1):
+                    try:
+                        new_key = (chrom_alt, pos_alt + i, event.chrom, event.pos + j)
+                        possible_reverse_keys.append(new_key)
+                    except TypeError:
+                        continue
 
-        # Check each reverse key to see if a mate exists in event_dict.
-        for reverse_key in possible_reverse_keys:
-            if reverse_key in event_dict:  # Means got a mate.
-                # Now iterating through all events associated with the reverse_key
-                for mate_event in event_dict[reverse_key]:
-                    # If a mate is found, add the pair to the mate_bnd_pairs list.
-                    mate_bnd_pairs.append((mate_event, event))
-                    # Remove the found mate event from the dictionary.
-                    event_dict[reverse_key].remove(mate_event)
-                    mate_found = True
-                    break  # Stop searching once a mate is found.
-                # If a mate was found and no events are left under the reverse key, remove the key.
-                if mate_found:
-                    if len(event_dict[reverse_key]) == 0:
-                        # Remove the key if no events are left
-                        del event_dict[reverse_key]
-                    break
+            mate_found = False
+            for reverse_key in possible_reverse_keys:
+                if reverse_key in event_dict:
+                    for mate_event in event_dict[reverse_key]:
+                        mate_bnd_pairs.append((mate_event, event))
+                        event_dict[reverse_key].remove(mate_event)
+                        mate_found = True
+                        break
+                    if mate_found:
+                        if len(event_dict[reverse_key]) == 0:
+                            del event_dict[reverse_key]
+                        break
 
-        if not mate_found:
-            if key not in event_dict:  # Create a new list if key doesn't exist.
-                event_dict[key] = []
-            event_dict[key].append(event)  # # Add the event to the list under its key.
+            if not mate_found:
+                if key not in event_dict:
+                    event_dict[key] = []
+                event_dict[key].append(event)
 
-    # Flattening the list of events from all keys
+        except (TypeError, ValueError) as e:
+            logging.info(f"Error processing event: {str(e)}")
+            continue
+
     return [single_event for event_list in event_dict.values() for single_event in event_list]
 
 
-def find_special_no_mate_diff_bnd_pair_and_other_single_tra(
-    events,
-    pos_tolerance=3,
-):  # Process no_mate_events
-    """Extract special no mate diff bnd pair and other single TRA events from no_mate_events."""
+def find_special_no_mate_diff_bnd_pair_and_other_single_tra(events, pos_tolerance=3):
+    """Extract special no mate diff bnd pair and other single TRA events."""
     event_dict = {}
     special_no_mate_diff_bnd_pair = []
     other_single_TRA = []
 
     for event in events:
-        chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
-        key = (event.chrom, event.pos, chrom_alt, pos_alt)
+        try:
+            chrom_alt, pos_alt = get_alt_chrom_pos(event.alt)
+            if chrom_alt is None or pos_alt is None:
+                continue
 
-        # Generate all possible reverse keys
-        possible_reverse_keys = [
-            (event.chrom, event.pos + i, chrom_alt, pos_alt + j)
-            for i in range(-pos_tolerance, pos_tolerance + 1)
-            for j in range(-pos_tolerance, pos_tolerance + 1)
-        ]
+            key = (event.chrom, event.pos, chrom_alt, pos_alt)
 
-        mate_found = False  # This is a flag
-        for reverse_key in possible_reverse_keys:
-            if reverse_key in event_dict:  # If key in dict.
-                special_no_mate_diff_bnd_pair.append(
-                    (event_dict.pop(reverse_key), event),
-                )  # event_dict.pop(reverse_key) will delete mate events from event_dic and output
-                mate_found = True
-                break
+            # Generate all possible reverse keys
+            possible_reverse_keys = []
+            for i in range(-pos_tolerance, pos_tolerance + 1):
+                for j in range(-pos_tolerance, pos_tolerance + 1):
+                    try:
+                        new_key = (event.chrom, event.pos + i, chrom_alt, pos_alt + j)
+                        possible_reverse_keys.append(new_key)
+                    except TypeError:
+                        continue
 
-        if not mate_found:
-            event_dict[key] = event
+            mate_found = False
+            for reverse_key in possible_reverse_keys:
+                if reverse_key in event_dict:
+                    special_no_mate_diff_bnd_pair.append(
+                        (event_dict.pop(reverse_key), event)
+                    )
+                    mate_found = True
+                    break
+
+            if not mate_found:
+                event_dict[key] = event
+
+        except (TypeError, ValueError) as e:
+            logging.info(f"Error processing event: {str(e)}")
+            continue
 
     other_single_TRA = list(event_dict.values())
-
     return special_no_mate_diff_bnd_pair, other_single_TRA
--- src/octopusv/converter/base.py
+++ src/octopusv/converter/base.py
@@ -1,6 +1,5 @@
 import logging
 import re
-
 from natsort import natsorted
 
 logging.basicConfig(level=logging.INFO)
@@ -8,7 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 class Converter:
     """This is an abstract base class for all converter classes.
-
     It provides a common interface for all converters.
     The `convert` method is a placeholder that needs to be overridden in each concrete converter class.
     """
@@ -20,21 +18,51 @@ class Converter:
 
 def get_bnd_pattern(alt):
     """Get the pattern of BND: t[p[, t]p], ]p]t, [p[t.
-
     This function uses regular expressions to match the ALT field,
-    considering possible multiple nucleotides in the sequences.
+    considering possible multiple nucleotides in the sequences and special chromosome names.
     """
-    # Define regex patterns for the four BND types
     patterns = {
-        "t[p[": r"^[ACGTNacgtn]+\[[^\[\]:]+:\d+\[$",
-        "t]p]": r"^[ACGTNacgtn]+\][^\[\]:]+:\d+\]$",
-        "[p[t": r"^\[[^\[\]:]+:\d+\[[ACGTNacgtn]+$",
-        "]p]t": r"^\][^\[\]:]+:\d+\][ACGTNacgtn]+$",
+        "t[p[": r"^[ACGTNacgtn]*\[[^\[\]]+:\d+\[$",
+        "t]p]": r"^[ACGTNacgtn]*\][^\[\]]+:\d+\]$",
+        "[p[t": r"^\[[^\[\]]+:\d+\[[ACGTNacgtn]*$",
+        "]p]t": r"^\][^\[\]]+:\d+\][ACGTNacgtn]*$",
     }
+
     for pattern_name, regex in patterns.items():
         if re.match(regex, alt):
             return pattern_name
     return None
+
+
+def get_alt_chrom_pos(alt):
+    """Extract chromosome and position from ALT field in VCF file.
+    This function handles both standard chromosome names and special ones like hs37d5.
+    """
+    try:
+        # Handle various BND format patterns
+        brackets = {'[', ']'}
+        if not any(b in alt for b in brackets):
+            return None, None
+
+        parts = re.split(r'[\[\]]', alt)
+        for part in parts:
+            if ':' in part:
+                chrom, pos = part.split(':')
+                try:
+                    return chrom, int(pos)
+                except ValueError:
+                    continue
+
+        logging.info(
+            f"Unexpected ALT format, it should be something like N]chr10:69650962] or with sequences: {alt}"
+        )
+        return None, None
+
+    except Exception as e:
+        logging.info(
+            f"Error parsing ALT field: {alt}, error: {str(e)}"
+        )
+        return None, None
 
 
 def is_same_bnd_event(event1, event2) -> bool:
@@ -42,7 +70,6 @@ def is_same_bnd_event(event1, event2) -> bool:
     qualified_pairings = [{"t[p[", "t]p]"}]
     event1_pattern = get_bnd_pattern(event1.alt)
     event2_pattern = get_bnd_pattern(event2.alt)
-
     return any({event1_pattern, event2_pattern} == pairing for pairing in qualified_pairings)
 
 
@@ -58,7 +85,6 @@ def is_independent_bnd_event(event1, event2):
     ]
     event1_pattern = get_bnd_pattern(event1.alt)
     event2_pattern = get_bnd_pattern(event2.alt)
-
     return any({event1_pattern, event2_pattern} == pairing for pairing in qualified_pairings)
 
 
@@ -72,7 +98,6 @@ def is_independent_special_bnd_event(event1, event2):
     ]
     event1_pattern = get_bnd_pattern(event1.alt)
     event2_pattern = get_bnd_pattern(event2.alt)
-
     return any({event1_pattern, event2_pattern} == pairing for pairing in qualified_pairings)
 
 
@@ -81,36 +106,16 @@ def is_SpecialNoMateDiffBndPairReciprocalTranslocation(event1, event2):
     qualified_pairings = [{"t[p[", "]p]t"}, {"t]p]", "[p[t"}]
     event1_pattern = get_bnd_pattern(event1.alt)
     event2_pattern = get_bnd_pattern(event2.alt)
-
     return any({event1_pattern, event2_pattern} == pairing for pairing in qualified_pairings)
 
 
 def compare_chromosomes(event1, event2):
     """Determine if the chromosome of event1 is smaller than the chromosome of event2 using natural sort order.
-
     Returns:
         bool: True if the chromosome of event1 is smaller or equal to the chromosome of event2, False otherwise.
     """
     original_order = [event1.chrom, event2.chrom]
-
     # Get the order when the chromosomes are naturally sorted
     sorted_order = natsorted(original_order)
-
     # Return True if the original order matches the sorted order
     return original_order == sorted_order
-
-
-def get_alt_chrom_pos(alt):
-    """Extract chromosome and position from ALT field in VCF file.
-
-    This function uses regular expressions to handle cases where there are multiple nucleotides.
-    """
-    pattern = re.compile(r"[ACGTNacgtn]*[\[\]]([^:\[\]]+):(\d+)[\[\]][ACGTNacgtn]*")
-    match = pattern.match(alt)
-    if match:
-        chrom_alt, pos_alt = match.groups()
-        return chrom_alt, int(pos_alt)
-    logging.info(
-        f"Unexpected ALT format, it should be something like N]chr10:69650962] or with sequences: {alt}",
-    )
-    return None, None
